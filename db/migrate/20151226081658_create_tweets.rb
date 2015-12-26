ad70e8d1a318a85897e891da251d4aa2e565af42
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-12-26 20:37:22 +0900

    crate tweet

--- db/migrate/20151226081658_create_tweets.rb
+++ db/migrate/20151226081658_create_tweets.rb
@@ -1,6 +1,12 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+      t.string :text
+      t.float :tweet_id, :unique => true
+      t.datetime :row_created_at
+      t.string :latitude
+      t.string :longitude
+      t.string :shrine
 
       t.timestamps null: false
     end
